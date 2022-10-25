ebec4e3b8d3b4d9b1ee7c61e7f13a7d9314ac8d1
Vinicius Sousa <vinnysousa12@gmail.com>
2022-10-25 13:37:37 -0300

    Tratando exceções

--- src/main/java/com/enterprise/course/services/UserService.java
+++ src/main/java/com/enterprise/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.enterprise.course.entities.User;
 import com.enterprise.course.repositories.UserRepository;
+import com.enterprise.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
